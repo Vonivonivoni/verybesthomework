9c8c85f834c0646ad2eb14fcb43d86f4456e7af3
Raghu Betina <raghu@firstdraft.com>
2018-01-10 10:41:33 +0000

    Direct association between dishes and favorites

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,11 @@
 class Favorite < ApplicationRecord
   # Direct associations
 
+  belongs_to :favorite_dish,
+             :required => false,
+             :class_name => "Dish",
+             :foreign_key => "dish_id"
+
   # Indirect associations
 
   # Validations
