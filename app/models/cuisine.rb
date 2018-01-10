5d81b5173405c0472216fae8aeb8094c408ed16c
Raghu Betina <raghu@firstdraft.com>
2018-01-10 10:41:33 +0000

    Direct association between venues and cuisines

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,6 +1,10 @@
 class Cuisine < ApplicationRecord
   # Direct associations
 
+  belongs_to :restaurant,
+             :class_name => "Venue",
+             :foreign_key => "restaurants_id"
+
   has_many   :dishes
 
   # Indirect associations
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,10 @@
 class Venue < ApplicationRecord
   # Direct associations
 
+  has_one    :cuisine,
+             :foreign_key => "restaurants_id",
+             :dependent => :nullify
+
   # Indirect associations
 
   # Validations
